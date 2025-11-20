33ffdec2c183f7b766776e6ff7c3d450115d7f6c
kyk9412 <kyk9412@hanmail.net>
2025-11-20 10:27:55 +0900

    Modify:상세페이지 링크변경

--- src/pages/author/AuthorPage.ts
+++ src/pages/author/AuthorPage.ts
@@ -84,7 +84,7 @@ async function loadUserPosts() {
 
         const article = `          
         <article class="border-b border-br-line py-4">
-          <a href="/src/pages/details/DetailsPage.html">
+          <a href="../src/pages/details/DetailsPage.html">
             <div class="underline decoration text-[13px] text-br-primary pb-[10px]">${post.extra.subTitle}</div>
             <h3 class="text-[17px] mt-[14px]">${post.title}</h3>
             
