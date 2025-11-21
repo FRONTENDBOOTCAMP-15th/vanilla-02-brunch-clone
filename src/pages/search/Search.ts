2265b62f0e0055d8c41b44be1d35d0a5d84e93c8
kyk9412 <kyk9412@hanmail.net>
2025-11-21 14:55:42 +0900

    Feat:검색어 입력중

--- src/pages/search/Search.ts
+++ src/pages/search/Search.ts
@@ -125,16 +125,46 @@ initTextTabButton();
 // 검색창 글자 입력시 보이기/숨기기
 document.addEventListener('DOMContentLoaded', () => {
   const buttons = document.querySelectorAll<HTMLButtonElement>('.tab-btn');
-  const title = document.querySelector<HTMLElement>('#title');
+  const title = document.querySelector<HTMLElement>('#title') as HTMLElement;
+  const titleText = document.querySelector<HTMLElement>('#popular-keywords-title');
+  const contentText = title?.querySelector('p');
   const contents = document.querySelectorAll<HTMLElement>('[data-content]');
   const searchInput = document.querySelector<HTMLInputElement>('#search-input');
+  const searchTitle = document.querySelector<HTMLTitleElement>('#search-title');
 
-  // 검색어 입력시 section영역 숨김
-  if (searchInput && title) {
+  // 원본 내용 저장
+  const originalTitle = titleText?.textContent || '';
+  const originalContent = contentText?.innerHTML || '';
+
+  // 검색어 입력시 내용 변경
+  if (searchInput && titleText && contentText) {
     searchInput.addEventListener('input', () => {
       if (searchInput.value.trim().length > 0) {
+        // 검색 중일 때 내용 변경
+
+        contentText.innerHTML = `'<span class="text-[20px] text-br-primary font-medium">${searchInput.value}</span>' 검색 중...`;
+      } else {
+        // 원래 내용으로 복원
+        titleText.textContent = originalTitle;
+        contentText.innerHTML = originalContent;
+      }
+    });
+  }
+
+  // 검색어 입력시 section영역 숨김
+
+  // 검색어 입력 그리고 타이틀 요소에
+  if (searchTitle && title) {
+    // enter 이벤트 리스너가 발생하면
+    searchTitle.addEventListener('enter', () => {
+      // 검색창의 값에 공백을 제거하고, 길이가 0보다 크면
+      if (searchTitle.value.trim().length > 0) {
+        // title에 숨김처리
+        titleText.textContent = '검색 결과';
         title.classList.add('hidden');
       } else {
+        // 그렇지 않으면 title에 보임처리
+
         title.classList.remove('hidden');
       }
     });
@@ -176,7 +206,7 @@ async function loadUserProfile() {
       return;
     }
 
-    // 여기서 data 선언됨!!
+    // 여기서 data 선언됨!! ✔
     const data = await getUserProfile(authorId);
 
     if (!data.ok) {
@@ -214,9 +244,62 @@ async function loadUserProfile() {
       articleList.innerHTML += article;
     });
   } catch (error) {
-    console.error('조회 실패:', error);
+    console.error('실패:', error);
+  }
+}
+// 실행
+loadUserProfile();
+
+// 4 작가 홈 API 호출
+async function getAuthorProfile(authorId: number) {
+  const api = getAxios(); // axios instance 생성
+  const res = await api.get(`/users/${authorId}`);
+  return res.data; // { ok, item } 반환
+}
+
+// 5. 상세 페이지 렌더링
+async function authorProfile() {
+  //
+  try {
+    // const authorId = Number(new URLSearchParams(window.location.search).get('_id'));
+
+    // if (!authorId) {
+    //   console.error('URL 에 _id 가 없습니다.');
+    //   return;
+    // }
+
+    // 여기서 data 선언됨!!
+    const data = await getAuthorProfile(authorId);
+
+    if (!data.ok) {
+      console.error('API 응답 ok:false');
+      return;
+    }
+
+    // 사용자 정보 렌더링
+    document.querySelector('#name')!.textContent = data.item.name;
+    document.querySelector('#job')!.textContent = data.item.extra?.job ?? '';
+
+    const image = document.querySelector('#image') as HTMLImageElement;
+    image.src = data.item.image;
+
+    // 게시글 목록 렌더링
+    const authorlist = document.querySelector('#author-list') as HTMLElement;
+
+    data.item.postList.forEach((user: any) => {
+      const article = `
+           <article>
+            <img id="image" src="" alt=" 이미지" class="w-12 h-12 rounded-full object-cover" hidden />
+            <h2 id="name" class="text-[16px] text-br-detailsTitle mb-3">${user.title}</h2>
+            <h3 id="job" class="text-[12px] text-br-contentSecondary mb-3">${user.content}</h3>
+          </article>
+      `;
+      authorlist.innerHTML += article;
+    });
+  } catch (error) {
+    console.error('실패:', error);
   }
 }
 
 // 실행
-loadUserProfile();
+authorProfile();
