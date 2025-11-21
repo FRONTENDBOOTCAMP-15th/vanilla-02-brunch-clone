2ef64ac0e292dc40d4bf8c7684304f1389a97d4d
Jisub <qkrwltjq2426@naver.com>
2025-11-21 23:20:14 +0900

    Refactor: min-w-[360px] 삭제

--- src/pages/home/Home.ts
+++ src/pages/home/Home.ts
@@ -58,7 +58,7 @@ function createBrunchCard(post: PostItem, index: number): HTMLElement {
   }
 
   const wrapper: HTMLLIElement = document.createElement('li');
-  wrapper.className = 'flex items-start gap-5 cursor-pointer min-w-[360px] border-b border-gray-100 pb-4 pt-4 last:border-b-0 last:pb-0 last:pt-4';
+  wrapper.className = 'flex items-start gap-5 cursor-pointer  border-b border-gray-100 pb-4 pt-4 last:border-b-0 last:pb-0 last:pt-4';
   wrapper.addEventListener('click', () => {
     window.location.href = `/src/pages/details/DetailsPage.html?_id=${post._id}`;
   });
@@ -202,7 +202,7 @@ async function renderTodayAuthorSection(post: UserInfo) {
   section.className = 'p-[25px] bg-white';
   //img가 없을 때는 <img> 태그를 넣지 않음
   section.innerHTML = `
-  <div class="flex items-start justify-between p-0 min-w-[360px] h-[100px]">
+  <div class="flex items-start justify-between p-0  h-[100px]">
   
   <div class="flex flex-col">
     
